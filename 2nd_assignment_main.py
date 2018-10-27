667b17691569d1ab35dee070aff834255945c896
ChoJaeO <39540532+ChoJaeO@users.noreply.github.com>
2018-10-27 22:42:44 +0900

    라인 트레이서 구현

--- 2nd_assignment_main.py
+++ 2nd_assignment_main.py
@@ -23,6 +23,51 @@ class myCar(object):
     # Complete the code to perform Second Assignment
     # =======================================================================
     def car_startup(self):
+        control = self.car.steering
+        control.center_alignment
+
+        variation = self.car.accelerator
+        variation.ready()
+
+        blacksensor = self.car.line_detector
+
+        variation.go_forward(30)
+        if blacksensor.is_center():
+            variation.go_forward(40)
+            time.sleep(0.1)
+            variation.go_forward(30)
+        if blacksensor.is_equal_status([1,0,0,0,0]):
+            control.turn_left(35)
+            time.sleep(0.1)
+            control.center_alignment
+        elif blacksensor.is_equal_status([1,1,0,0,0]):
+            control.turn_left(30)
+            time.sleep(0.1)
+            control.center_alignment
+        elif blacksensor.is_equal_status([0,1,0,0,0]):
+            control.turn_left(10)
+            time.sleep(0.1)
+            control.center_alignment
+        elif blacksensor.is_equal_status([0,1,1,0,0]):
+            control.turn_left(5)
+            time.sleep(0.1)
+            control.center_alignment
+        elif blacksensor.is_equal_status([0,0,1,1,0])
+            control.turn_right(5)
+            time.sleep(0.1)
+            control.center_alignment
+        elif blacksensor.is_equal_status([0,0,0,1,0]):
+            control.turn_right(10)
+            time.sleep(0.1)
+            control.center_alignment
+        elif blacksensor.is_equal_status([0,0,0,1,1]):
+            control.turn_right(30)
+            time.sleep(0.1)
+            control.center_alignment
+        elif blacksensor.is_equal_status([0,0,0,0,1]):
+            control.turn_right(35)
+            time.sleep(0.1)
+            control.center_alignment
         # implement the assignment code here
         pass
 
